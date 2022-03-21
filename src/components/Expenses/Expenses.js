a508168d398d5dc26fd69f08eb2f4b1f13c741cc
Jayson Alihassan Diaz <jaysonprincediaz@gmail.com>
2022-03-21 10:57:23 +0800

    Initial Commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
